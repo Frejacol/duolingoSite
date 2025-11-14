a92040d8d86b45a96164234849967d4f09a193ec
Freja Colsted <frco0001@stud.ek.dk>
2025-11-14 21:53:33 +0100

    Forsiden godt i gang

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function updateInfoboxDay() {
   console.log("updateInfoboxDay");
   document.querySelector(".info-text .title").textContent = "Har du misset en dag?";
   document.querySelector(".info-text .placeholder").innerHTML = "<p><strong>Her er hvad du skal gøre hvis du har misset at lave din sprog lektion i en dag.</stong></p>";
-  document.querySelector("#efficiency").innerHTML = "<h2>Trusslesniveau</h2>";
+  document.querySelector("#efficiency").innerHTML = "<h2>Trusslesniveau</h2><div class='flames'><img class='flame' src='img/flame.png' alt='flame'></div>";
   document.querySelector("#requirement").innerHTML = "<h2>Hvad gør du nu?</h2><ul><li>Info</li><li>Info</li><li>Info</li><li>Info</li><li>Info</li></ul>";
 }
 
@@ -14,7 +14,7 @@ function updateInfoboxWeek() {
   console.log("updateInfoboxWeek");
   document.querySelector(".info-text .title").textContent = "Har du misset en uge?";
   document.querySelector(".info-text .placeholder").innerHTML = "<p><strong>Her er hvad du skal gøre hvis du har misset at lave dine sprog lektioner i en uge.</stong></p>";
-  document.querySelector("#efficiency").innerHTML = "<h2>Trusslesniveau</h2>";
+  document.querySelector("#efficiency").innerHTML = "<h2>Trusslesniveau</h2><div class='flames'><img class='flame' src='img/flame.png' alt='flame'><img class='flame' src='img/flame.png' alt='flame'></div>";
   document.querySelector("#requirement").innerHTML = "<h2>Hvad gør du nu?</h2><ul><li>Info</li><li>Info</li><li>Info</li><li>Info</li><li>Info</li></ul>";
 }
 
@@ -22,6 +22,7 @@ function updateInfoboxMonth() {
   console.log("updateInfoboxMonth");
   document.querySelector(".info-text .title").textContent = "Har du misset en måned?";
   document.querySelector(".info-text .placeholder").innerHTML = "<p><strong>Her er hvad du skal gøre hvis du har misset at lave dine sprog lektioner i en måned.</stong></p>";
-  document.querySelector("#efficiency").innerHTML = "<h2>Trusslesniveau</h2>";
+  document.querySelector("#efficiency").innerHTML =
+    "<h2>Trusslesniveau</h2><div class='flames'><img class='flame' src='img/flame.png' alt='flame'><img class='flame' src='img/flame.png' alt='flame'><img class='flame' src='img/flame.png' alt='flame'></div>";
   document.querySelector("#requirement").innerHTML = "<h2>Hvad gør du nu?</h2><ul><li>Info</li><li>Info</li><li>Info</li><li>Info</li><li>Info</li></ul>";
 }
